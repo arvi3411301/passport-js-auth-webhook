b05244512425c5170644d0156d7baedc1b2e454c
Aravind Shankar <aravind@hasura.io>
2018-09-18 15:59:12 +0530

    added DATABASE_URL env

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
 // Update with your config settings.
 
-const databaseName = "postgres"
-const pg = require('pg')
+const databaseName = "postgres";
+const pg = require('pg');
+
+const connection_url = process.env.DATABASE_URL || `postgres://postgres:@localhost:5432/${databaseName}`;
 
 module.exports = {
   client: 'pg',
-  connection: `postgres://postgres:@localhost:5432/${databaseName}`,
+  connection: connection_url,
   migrations: {
     directory: __dirname + '/db/migrations'
   }
